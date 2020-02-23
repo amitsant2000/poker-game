497893625c688aa01f3c7341df336dd5421b0e51
Rukshad Kasad <rkasad@berkeley.edu>
2020-02-22 18:31:40 -0800

    condensed deck constructor

--- src/Deck.java
+++ src/Deck.java
@@ -5,13 +5,15 @@ class Deck {
 
     public Deck() {
         _deck = new ArrayList<>();
-        char[] suits = {'D', 'C', 'H', 'S'};
-        for (int nSuits = 0; nSuits < 4; nSuits++) {
-            char suit = suits[nSuits];
+        for (char suit : new char[] {'D', 'C', 'H', 'S'}) {
             for (int cVal = 0; cVal < 13; cVal++) {
                 _deck.add(new Card(cVal, suit));
             }
         }
+    }
+
+    public void shuffle() {
+
     }
 
 
